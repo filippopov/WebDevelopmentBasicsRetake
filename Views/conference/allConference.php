d3a1f91ff2780eed0e4f66f25bbee175286ec9f3
filippopov <filip_popov@mail.bg>
2015-11-24 01:36:06 +0200

    add bootstrap to view

--- Views/conference/allConference.php
+++ Views/conference/allConference.php
@@ -1,19 +1,29 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <?php /** @var \MVC\ViewModels\ConferenceViewModel[] $model */?>
-<a href="http://localhost:8004/Web-Development-Basics-Retake/users/profile">Back To Profile</a>
-<a href="http://localhost:8004/Web-Development-Basics-Retake/conference/addconference">Add Conference</a>
+
+<div class="container">
+    <div class="tab-content">
+        <div class="tab-pane active" id="daftar">
+            <br/>
+            <p><a class="btn btn-success"href="http://localhost:8004/Web-Development-Basics-Retake/users/profile">Back To Profile</a>
+            <a class="btn btn-primary" href="http://localhost:8004/Web-Development-Basics-Retake/conference/addconference"><span class="glyphicon glyphicon-plus"></span>Add Conference</a></p>
+            <br/>
+            <div class="table-responsive">
 <?php
 
 \MVC\ViewHelpers\GenerateTable::create()->create()
-    ->addAttribute('id', 'names')
-    ->addAttribute('class', 'red-menu')
-    ->addAttribute('border','1px')
+    ->addAttribute('class','table table-hover')
     ->setHeaders(['Id','Conference Name','Creator Name','Start Time','End Time','Count of Breaks','Halls Name','Status Name','Delete'])
     ->setContentConference($model)
     ->render();
 
 ?>
-
+            </div>
+            </div>
+            </div>
+</div>
 
 <script>
     $('a').click(function(e){
--- Views/users/register.php
+++ Views/users/register.php
@@ -1,9 +1,51 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"/>
 <?= $model->error ? $model->error : '' ;?>
 
-<form action="" method="post">
-    <input type="text" name="username" />
-    <input type="password" name="password" />
-    <input type="submit" value="Register" />
-</form>
+<!--<form action="" method="post">-->
+<!--    <input type="text" name="username" />-->
+<!--    <input type="password" name="password" />-->
+<!--    <input type="submit" value="Register" />-->
+<!--</form>-->
+<!---->
+<!--<a href="http://localhost:8004/Web-Development-Basics-Retake/users/login">Login</a>-->
 
-<a href="http://localhost:8004/Web-Development-Basics-Retake/users/login">Login</a>
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="page-header" style="margin-top:5px;">
+                    <h3>Register Area</h3>
+                </div>
+                <form class="form-horizontal" role="form" method="post">
+                    <div class="form-group">
+                        <label for="inputfn3" class="col-sm-2 control-label">Username</label>
+                        <div class="col-sm-10">
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                                <input type="text" name="username" class="form-control" id="inputfn3" placeholder="Username">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                        <div class="col-sm-10">
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-star"></span></span>
+                                <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <input type="submit" class="btn btn-primary" value="Register" />
+                            <a href="http://localhost:8004/Web-Development-Basics-Retake/users/login" type="button" class="btn btn-success">Back to Login</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"></div>
+</div>
